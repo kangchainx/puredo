68597051f2b6a0fef40e80e4a4db4d5a785ce0a3
chriskang <kangchenhe666@gmail.com>
2026-02-01 12:25:26 +0800

    Initial Commit

---
+++ Puredo/PuredoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuredoApp.swift
+//  Puredo
+//
+//  Created by Chris Kang on 2026/2/1.
+//
+
+import SwiftUI
+
+@main
+struct PuredoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
